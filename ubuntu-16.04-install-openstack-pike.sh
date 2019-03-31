c6fce4ce1eb8d4170a8c0920b46fe4a731b59762
lymweb <>
2019-03-31 13:43:15 +0800

    Update ubuntu-16.04-install-openstack-pike.sh

--- ubuntu-16.04-install-openstack-pike.sh
+++ ubuntu-16.04-install-openstack-pike.sh
@@ -29,7 +29,6 @@ PROVIDER_NETWORK_CIDR='10.10.26.0/23'
 RABBIT_PASS='yourpassword'
 METADATA_SECRET='yourpassword' #/etc/neutron/metadata_agent.ini suitable secret for the metadata proxy
 
-
 #qemu or kvm
 VIRT_TYPE="kvm"
 
